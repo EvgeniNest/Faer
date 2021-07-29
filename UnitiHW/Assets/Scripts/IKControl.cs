b5f8397a029964500ce51e5ec3c772791623bd11
evgeninest <zheka572@mail.ru>
2021-07-29 23:06:01 +0300

    HW5

---
+++ UnitiHW/Assets/Scripts/IKControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IKControl : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isActive;
+    [SerializeField]
+    private Transform _lookObject;
+    [SerializeField]
+    private Transform _pointHandObject;
+    [SerializeField]
+    private float _valueWeight;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (_isActive)
+        {            
+             _animator.SetLookAtWeight(_valueWeight);
+             _animator.SetLookAtPosition(_lookObject.position);
+
+             ChangeWeightRightHand(_valueWeight);
+
+             _animator.SetIKPosition(AvatarIKGoal.RightHand, _pointHandObject.position);
+             _animator.SetIKRotation(AvatarIKGoal.RightHand, _pointHandObject.rotation);
+        }
+        else
+        {
+            ChangeWeightRightHand(0);
+            _animator.SetLookAtWeight(0);
+        }
+    }  
+
+private void ChangeWeightRightHand(float value)
+    {
+        _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, value);
+        _animator.SetIKRotationWeight(AvatarIKGoal.RightHand, value);
+    }
+     
+}
